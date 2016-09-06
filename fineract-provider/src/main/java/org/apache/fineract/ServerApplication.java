b5c3af317b12fca338bb6936e18e401e34650165
habile <habile@habile-PC>
2016-09-06 18:32:27 +0530

    hi

--- fineract-provider/src/main/java/org/apache/fineract/ServerApplication.java
+++ fineract-provider/src/main/java/org/apache/fineract/ServerApplication.java
@@ -33,7 +33,7 @@ public class ServerApplication {
 	public static void main(String[] args) throws Exception {
 		ConfigurableApplicationContext ctx = SpringApplication.run(Configuration.class, args);
 		ApplicationExitUtil.waitForKeyPressToCleanlyExit(ctx);
-		System.out.println("hi");
+		System.out.println("hi1");
 	}
 
 }
